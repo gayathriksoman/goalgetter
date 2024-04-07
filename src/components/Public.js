f8efc67168c26e3544e5419047202b3816501865
gayathriksoman <gayathrikooniyath@gmail.com>
2024-04-08 01:09:57 +0530

    [g3] home page upgrade

--- src/components/Public.js
+++ src/components/Public.js
@@ -7,20 +7,15 @@ const Public = () => {
                 <h1>Welcome to <span className="nowrap">GoalGetter!</span></h1>
             </header>
             <main className="public__main">
-                <p>
-                    🌟 Ready to turn your goals into reality? Welcome to Goal Getter—the ultimate hub for goal-setters like you! 🚀💪
-                </p>
-                <p>
-                    👯‍♀️ Dive into a community where your dreams are celebrated and your progress is cheered on. With Goal Getter, it's not just about YOUR goals—it's about supporting each other to reach new heights. 🌈✨
-                </p>
-                <p>
-                    💥 Get inspired by seeing what your peers are working towards and join forces to crush your goals together! 🤝 Let's make every step count. Join the movement now! #GoalGetter #DreamBigAchieveBigger 💫📈
-                </p>
+                <h2>Make Progress. Make Connections. Grow Together.</h2>
+                <button>
+                <Link to="/login">Login</Link>
+                </button>
                 
 
             </main>
             <footer>
-                <Link to="/login">Login</Link>
+                Created by KS.
             </footer>
         </section>
 
