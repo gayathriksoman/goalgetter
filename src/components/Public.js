0736e52b4615ba39906ba5f51c771295b9bb20bf
gayathriksoman <gayathrikooniyath@gmail.com>
2024-04-09 17:41:22 +0530

    [g3] home catto png

--- src/components/Public.js
+++ src/components/Public.js
@@ -7,7 +7,7 @@ const Public = () => {
                 <h1>Welcome to <span className="nowrap">GoalGetter!</span></h1>
             </header>
             <main className="public__main">
-                <img className="home_catto" src={`${process.env.PUBLIC_URL}/home_catto.webp`} alt="catto" />
+                <img className="home_catto" src={`${process.env.PUBLIC_URL}/home_catto.png`} alt="catto" />
                 <h2>Make Progress. Make Connections. Grow Together.</h2>
                 <button>
                 <Link to="/login">Login</Link>
